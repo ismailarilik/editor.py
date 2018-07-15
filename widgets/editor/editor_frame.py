c113558cbf1133e1bac307f13d3bcaacefa8e8e6
İsmail Arılık <arilik.ismail@gmail.com>
2018-07-15 18:50:55 +0300

    Add text widget to editor frame

--- widgets/editor/editor_frame.py
+++ widgets/editor/editor_frame.py
@@ -4,3 +4,6 @@ from .text_widget import TextWidget
 class EditorFrame(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
+        # Add text widget
+        self.text_widget = TextWidget(self)
+        self.text_widget.pack(fill=tk.BOTH, expand=True)
---
+++ widgets/editor/text_widget.py
@@ -0,0 +1,5 @@
+import tkinter as tk
+
+class TextWidget(tk.Text):
+    def __init__(self, master=None):
+        super().__init__(master)
