ee8df000af56c9d77fb7b9808da6bc46d2a42963
İsmail Arılık <arilik.ismail@gmail.com>
2018-07-15 19:06:39 +0300

    Add file menu

---
+++ menus/file_menu.py
@@ -0,0 +1,5 @@
+import tkinter as tk
+
+class FileMenu(tk.Menu):
+    def __init__(self, master=None):
+        super().__init__(master)
--- menus/main_menu.py
+++ menus/main_menu.py
@@ -1,5 +1,9 @@
 import tkinter as tk
+from .file_menu import FileMenu
 
 class MainMenu(tk.Menu):
     def __init__(self, master=None):
         super().__init__(master)
+        # Add file menu
+        self.file_menu = tk.Menu(self)
+        self.add_cascade(label='File', menu=self.file_menu)
