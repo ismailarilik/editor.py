604fde480cde6ffb3f341234acbdbb03ea036469
İsmail Arılık <arilik.ismail@gmail.com>
2018-07-15 20:27:26 +0300

    Save opened file path

--- menus/file_menu.py
+++ menus/file_menu.py
@@ -5,12 +5,13 @@ class FileMenu(tk.Menu):
     def __init__(self, master, main_frame):
         super().__init__(master)
         self.main_frame = main_frame
+        self.opened_file_path = None
         # Add open file command
         self.add_command(label='Open File', command=self.open_file)
 
     def open_file(self):
-        file_name = tk_filedialog.askopenfilename()
-        if file_name:
+        self.opened_file_path = tk_filedialog.askopenfilename()
+        if self.opened_file_path:
             # Set editor text with file content
-            with open(file_name, encoding='UTF-8') as file:
+            with open(self.opened_file_path, encoding='UTF-8') as file:
                 self.main_frame.editor_frame.set_text(file.read())
