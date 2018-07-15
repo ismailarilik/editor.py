48efa926a58ea6b1d0bca8c551d85679603f08fc
İsmail Arılık <arilik.ismail@gmail.com>
2018-07-15 17:59:24 +0300

    Add main frame

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from main_frame import MainFrame
 
 class App(object):
     def __init__(self):
@@ -7,6 +8,8 @@ class App(object):
         self.window.title('Visual Python')
         # Set window icon
         self.window.iconbitmap('icon.ico')
+        # Add main frame
+        self.main_frame = MainFrame(self.window)
 
     def start(self):
         self.window.mainloop()
