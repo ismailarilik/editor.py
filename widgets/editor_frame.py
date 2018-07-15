b9c8487cfe69fc30622ff1d9ae13897063bdb5b5
İsmail Arılık <arilik.ismail@gmail.com>
2018-07-15 18:40:18 +0300

    Add editor frame

---
+++ widgets/editor_frame.py
@@ -0,0 +1,5 @@
+import tkinter as tk
+
+class EditorFrame(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
--- widgets/main_frame.py
+++ widgets/main_frame.py
@@ -1,5 +1,9 @@
 import tkinter as tk
+from .editor_frame import EditorFrame
 
 class MainFrame(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
+        # Add editor frame
+        self.editor_frame = EditorFrame(self)
+        self.editor_frame.pack(fill=tk.BOTH, expand=True)
