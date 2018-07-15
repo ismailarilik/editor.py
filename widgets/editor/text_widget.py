f0a7d60dd85699fcacc6ba322a93444be415e9eb
İsmail Arılık <arilik.ismail@gmail.com>
2018-07-15 21:19:44 +0300

    Enable undo feature for editor text widget

--- widgets/editor/text_widget.py
+++ widgets/editor/text_widget.py
@@ -2,7 +2,7 @@ import tkinter as tk
 
 class TextWidget(tk.Text):
     def __init__(self, master):
-        super().__init__(master, wrap=tk.NONE)
+        super().__init__(master, undo=True, wrap=tk.NONE)
         # Set vertical and horizontal scrollbars
         vertical_scrollbar = tk.Scrollbar(self.master)
         vertical_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
