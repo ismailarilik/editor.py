71b7c7a87daa2f513905f040d186f3b78ba2e5ca
İsmail Arılık <arilik.ismail@gmail.com>
2018-07-15 21:18:10 +0300

    Set scrollbars of editor text widget

--- widgets/editor/text_widget.py
+++ widgets/editor/text_widget.py
@@ -3,6 +3,14 @@ import tkinter as tk
 class TextWidget(tk.Text):
     def __init__(self, master):
         super().__init__(master, wrap=tk.NONE)
+        # Set vertical and horizontal scrollbars
+        vertical_scrollbar = tk.Scrollbar(self.master)
+        vertical_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        vertical_scrollbar.config(command=self.yview)
+        horizontal_scrollbar = tk.Scrollbar(self.master, orient=tk.HORIZONTAL)
+        horizontal_scrollbar.pack(side=tk.BOTTOM, fill=tk.X)
+        horizontal_scrollbar.config(command=self.xview)
+        self.config(yscrollcommand=vertical_scrollbar.set, xscrollcommand=horizontal_scrollbar.set)
 
     def get_wo_eol(self):
         '''
