cabf8e13c833abf9d06ce21ca83d8f8256a6d5d2
İsmail Arılık <arilik.ismail@gmail.com>
2018-07-15 17:53:00 +0300

    Set window title

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import tkinter as tk
 class App(object):
     def __init__(self):
         self.window = tk.Tk()
+        # Set window title
+        self.window.title('Visual Python')
 
     def start(self):
         self.window.mainloop()
