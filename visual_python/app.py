3c4d552ac1d24892d24a9bb2abd4e4112f8c37d6
İsmail Arılık <arilik.ismail@gmail.com>
2018-07-19 20:32:29 +0300

    Warn user about unsaved changes on quit

--- visual_python/app.py
+++ visual_python/app.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import tkinter.messagebox as tk_messagebox
 from .widgets.main_frame import MainFrame
 from .menus.main_menu import MainMenu
 
@@ -11,6 +12,8 @@ class App(tk.Tk):
         self.title(f'(unsaved_file) - {self.app_title}')
         # Set app icon
         self.iconbitmap('icon.ico')
+        # Register delete window protocol to handle things properly on quit
+        self.protocol("WM_DELETE_WINDOW", self.on_quit)
         # Add main frame
         self.main_frame = MainFrame(self, self)
         self.main_frame.pack(fill=tk.BOTH, expand=True)
@@ -20,3 +23,22 @@ class App(tk.Tk):
 
     def start(self):
         self.mainloop()
+
+    def on_quit(self):
+        # If there are unsaved changes, warn user about that
+        if self.title().startswith('*'):
+            reply = tk_messagebox.askyesnocancel(
+                'Unsaved Changes',
+                'There are unsaved changes, would you like to save them?',
+                parent=self
+            )
+            # If user choose to save, save the file and destroy the app
+            # If user choose not to save, just destroy the app
+            # If user cancel quitting, do nothing
+            if reply:
+                self.main_menu.file_menu.save_file()
+                self.destroy()
+            elif reply == False:
+                self.destroy()
+            else:
+                pass
