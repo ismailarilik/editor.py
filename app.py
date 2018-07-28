210647b28a94ccd3ce93657c68507723fa8ea3f9
İsmail Arılık <arilik.ismail@gmail.com>
2018-07-28 16:01:51 +0300

    Add a method to get previous tab stop

--- app.py
+++ app.py
@@ -153,6 +153,17 @@ class Editor(tk.Text):
         self.delete('1.0', tk.END)
         self.insert(tk.END, text)
 
+    def _get_previous_tab_stop(self, index):
+        '''
+        Get previous tab stop for the case of which tab_type is SPACE
+        '''
+        row, col = index.split('.')
+        if col == '0':
+            return index
+        excess_spaces = int(col) % self.tab_size
+        previous_tab_stop_col = int(col) - (excess_spaces if excess_spaces != 0 else self.tab_size)
+        return f'{row}.{previous_tab_stop_col}'
+
     def _insert_indentation(self):
         if self.tab_type is self.TabType.SPACE:
             self.insert(tk.INSERT, ' ' * self.tab_size)
