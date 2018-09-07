4431929291cd89b706f715654ff8f707c639b4ce
İsmail Arılık <arilik.ismail@gmail.com>
2018-09-07 19:09:40 +0300

    Add open file functionality

--- window.py
+++ window.py
@@ -1,5 +1,8 @@
 import tkinter as tk
 import tkinter.font as tk_font
+import tkinter.filedialog as tk_filedialog
+import os
+import tokenize
 
 class Editor(tk.Text):
 	def __init__(self, master):
@@ -17,17 +20,41 @@ class Editor(tk.Text):
 		tab_width = font.measure(' ' * self._tab_size)
 		self.config(tabs=(tab_width,))
 
+	def set(self, text):
+		self.delete('1.0', tk.END)
+		self.insert(tk.END, text)
+
+class File(object):
+	def __init__(self, path):
+		self.path = path
+
+	@property
+	def name(self):
+		return os.path.basename(self.path)
+
 class Window(tk.Tk):
 	def __init__(self):
 		super().__init__()
+		self.file = None
 		# Create editor
 		self.editor = Editor(self)
 		self.editor.pack()
+		# Add keyboard bindings for opening file
+		self.bind('<Control-KeyPress-o>', self.open_file)
+		self.bind('<Control-KeyPress-O>', self.open_file)
 		# Add keyboard bindings for saving file
 		self.bind('<Control-KeyPress-s>', self.save_file)
 		self.bind('<Control-KeyPress-S>', self.save_file)
 		# Start window
 		self.mainloop()
+
+	def open_file(self, event=None):
+		file_path = tk_filedialog.askopenfilename(filetypes=[('Python Files', '.py')])
+		if file_path:
+			self.file = File(file_path)
+			# Set editor text with file text
+			with tokenize.open(self.file.path) as file:
+				self.editor.set(file.read())
 
 	def save_file(self, event=None):
 		with open('C:\\Users\\ismail.arilik\\Development\\visual-python\\window.py', 'w', encoding='UTF-8') as file:
