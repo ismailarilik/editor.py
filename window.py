fc87d5edca16b7fb1bbaf837dbdb79b695ec8855
İsmail Arılık <arilik.ismail@gmail.com>
2018-09-08 14:01:40 +0300

    Add scrollbars to editor

--- window.py
+++ window.py
@@ -9,6 +9,7 @@ class Editor(tk.Text):
 	def __init__(self, master):
 		super().__init__(master, undo=True, wrap=tk.NONE)
 		self.tab_size = 4
+		self.add_scrollbars()
 
 	@property
 	def tab_size(self):
@@ -31,6 +32,18 @@ class Editor(tk.Text):
 	def set(self, text):
 		self.delete('1.0', tk.END)
 		self.insert(tk.END, text)
+
+	def add_scrollbars(self):
+		'''
+		Add vertical and horizontal scrollbars
+		'''
+		vertical_scrollbar = tk.Scrollbar(self.master)
+		vertical_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+		vertical_scrollbar.config(command=self.yview)
+		horizontal_scrollbar = tk.Scrollbar(self.master, orient=tk.HORIZONTAL)
+		horizontal_scrollbar.pack(side=tk.BOTTOM, fill=tk.X)
+		horizontal_scrollbar.config(command=self.xview)
+		self.config(yscrollcommand=vertical_scrollbar.set, xscrollcommand=horizontal_scrollbar.set)
 
 class File(object):
 	def __init__(self, path, is_modified=False):
