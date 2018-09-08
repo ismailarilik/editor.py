bbf87acc9bd3fe20a92afb1ba86ec775a1203197
İsmail Arılık <arilik.ismail@gmail.com>
2018-09-08 14:15:06 +0300

    Add highlighting support to editor

--- window.py
+++ window.py
@@ -2,14 +2,109 @@ import tkinter as tk
 import tkinter.filedialog as tk_filedialog
 import tkinter.font as tk_font
 import tkinter.messagebox as tk_messagebox
+import io
+import keyword
 import os
 import tokenize
 
+class Token(object):
+	def __init__(self, _type, exact_type, name, string, start_row, start_column, end_row, end_column, line):
+		self.type = _type
+		self.exact_type = exact_type
+		self.name = name
+		self.string = string
+		self.start_row = start_row
+		self.start_column = start_column
+		self.end_row = end_row
+		self.end_column = end_column
+		self.line = line
+
+class Tokenizer(object):
+	# Initialize keyword token type and name
+	KEYWORD = 1000
+	_KEYWORD_NAME = 'KEYWORD'
+
+	def __init__(self):
+		self._keywords = keyword.kwlist
+
+	def get_token_name(self, token_type):
+		if token_type == self.KEYWORD:
+			return self._KEYWORD_NAME
+		return tokenize.tok_name[token_type]
+
+	def tokenize(self, readline):
+		tokens = tokenize.tokenize(readline)
+		for token in tokens:
+			token2 = Token(token.type, token.exact_type, tokenize.tok_name[token.exact_type], token.string, token.start[0], token.start[1], token.end[0], token.end[1], token.line)
+			# Check for keyword token
+			if token2.exact_type == tokenize.NAME and token2.string in self._keywords:
+				token2.exact_type = token2.type = self.KEYWORD
+				token2.name = self._KEYWORD_NAME
+			yield token2
+
 class Editor(tk.Text):
-	def __init__(self, master):
+	def __init__(self, master, window):
 		super().__init__(master, undo=True, wrap=tk.NONE)
+		self.window = window
 		self.tab_size = 4
+		self.tokenizer = Tokenizer()
+		self.token_type_color_map = {
+			self.tokenizer.KEYWORD: '#FF0000',
+			tokenize.STRING: '#00C000',
+			tokenize.NUMBER: '#0000FF',
+			tokenize.COMMENT: '#808080',
+			tokenize.LPAR: '#FFC000',
+			tokenize.RPAR: '#FFC000',
+			tokenize.LSQB: '#FF00FF',
+			tokenize.RSQB: '#FF00FF',
+			tokenize.LBRACE: '#00C0C0',
+			tokenize.RBRACE: '#00C0C0',
+			tokenize.EQUAL: '#800000',
+			tokenize.PLUSEQUAL: '#800000',
+			tokenize.MINEQUAL: '#800000',
+			tokenize.STAREQUAL: '#800000',
+			tokenize.DOUBLESTAREQUAL: '#800000',
+			tokenize.SLASHEQUAL: '#800000',
+			tokenize.DOUBLESLASHEQUAL: '#800000',
+			tokenize.PERCENTEQUAL: '#800000',
+			tokenize.ATEQUAL: '#800000',
+			tokenize.VBAREQUAL: '#800000',
+			tokenize.AMPEREQUAL: '#800000',
+			tokenize.CIRCUMFLEXEQUAL: '#800000',
+			tokenize.LEFTSHIFTEQUAL: '#800000',
+			tokenize.RIGHTSHIFTEQUAL: '#800000',
+			tokenize.PLUS: '#000080',
+			tokenize.MINUS: '#000080',
+			tokenize.STAR: '#000080',
+			tokenize.DOUBLESTAR: '#000080',
+			tokenize.SLASH: '#000080',
+			tokenize.DOUBLESLASH: '#000080',
+			tokenize.PERCENT: '#000080',
+			tokenize.AT: '#000080',
+			tokenize.VBAR: '#000080',
+			tokenize.AMPER: '#000080',
+			tokenize.TILDE: '#000080',
+			tokenize.CIRCUMFLEX: '#000080',
+			tokenize.LEFTSHIFT: '#000080',
+			tokenize.RIGHTSHIFT: '#000080',
+			tokenize.LESS: '#808000',
+			tokenize.GREATER: '#808000',
+			tokenize.EQEQUAL: '#808000',
+			tokenize.NOTEQUAL: '#808000',
+			tokenize.LESSEQUAL: '#808000',
+			tokenize.GREATEREQUAL: '#808000',
+			tokenize.DOT: '#800080',
+			tokenize.COMMA: '#8000FF',
+			tokenize.COLON: '#8080FF',
+			tokenize.SEMI: '#FF0080',
+			tokenize.RARROW: '#FF8080',
+			tokenize.ELLIPSIS: '#FF80FF'
+		}
 		self.add_scrollbars()
+		# Set a flag to ensure modified callback being called only by a change
+		self.modified_event_occurred_by_change = True
+		# Listen for modified event
+		self.bind('<<Modified>>', self.modified)
 		# Handle open file event here, too, for this widget and prevent propagation of event
 		# Because default behavior of this widget is not wanted here
 		self.bind('<Control-KeyPress-o>', self.handle_open_file_event_and_prevent_propagation)
@@ -47,6 +142,40 @@ class Editor(tk.Text):
 		horizontal_scrollbar.pack(side=tk.BOTTOM, fill=tk.X)
 		horizontal_scrollbar.config(command=self.xview)
 		self.config(yscrollcommand=vertical_scrollbar.set, xscrollcommand=horizontal_scrollbar.set)
+
+	def modified(self, event):
+		if self.modified_event_occurred_by_change:
+			self.highlight()
+			# File is modified now
+			self.window.menu.file_menu.file.is_modified = True
+			# Reset title because unsaved changes status has been changed to True
+			title = self.window.get_title()
+			title.is_there_unsaved_change = self.window.menu.file_menu.file.is_modified
+			self.window.set_title(title)
+			# Call this method to set modified flag to False so following modification may cause modified event occurred
+			self.edit_modified(False)
+		# Switch this flag which is for to ensure modified callback being called only by a change
+		# Because changing modified flag above causes modified event occurred
+		self.modified_event_occurred_by_change = not self.modified_event_occurred_by_change
+
+	def highlight(self):
+		# Remove previous tags, otherwise they will conflict with new ones
+		self.tag_delete(*self.tag_names())
+		# Configure tag colors
+		for token_type, token_color in self.token_type_color_map.items():
+			self.tag_config(self.tokenizer.get_token_name(token_type), foreground=token_color)
+		text = self.get('1.0', tk.END)
+		try:
+			tokens = self.tokenizer.tokenize(io.BytesIO(text.encode('UTF-8')).readline)
+			for token in tokens:
+				# If there is a configured tag for this token, add it to the token's indices in the editor
+				color = self.token_type_color_map.get(token.exact_type)
+				if color:
+					start_index = f'{token.start_row}.{token.start_column}'
+					end_index = f'{token.end_row}.{token.end_column}'
+					self.tag_add(token.name, start_index, end_index)
+		except:
+			pass
 
 	def handle_open_file_event_and_prevent_propagation(self, event=None):
 		self.window.menu.file_menu.open_file(event)
@@ -186,7 +315,7 @@ class Window(tk.Tk):
 		# Set icon
 		self.iconbitmap('icon.ico')
 		# Create editor
-		self.editor = Editor(self)
+		self.editor = Editor(self, self)
 		self.editor.pack(fill=tk.BOTH, expand=True)
 		# Add menu
 		self.menu = Menu(self, self)
