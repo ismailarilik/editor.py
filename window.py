3a0132b8dd84ce73d2b9508c936994c2ad63bdab
İsmail Arılık <arilik.ismail@gmail.com>
2018-09-10 21:05:55 +0300

    Do not expose is_modified property of File, initialize it inside

--- window.py
+++ window.py
@@ -258,9 +258,9 @@ class MainFrame(tk.Frame):
 		self.find_frame = FindFrame(self, self.window)
 
 class File(object):
-	def __init__(self, path, is_modified=False):
+	def __init__(self, path):
 		self.path = path
-		self.is_modified = is_modified
+		self.is_modified = False
 
 	@property
 	def name(self):
