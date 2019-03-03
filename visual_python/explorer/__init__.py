de0ac526fa46cd846d1897036bdf94030521de13
İsmail Arılık <arilik.ismail@gmail.com>
2019-03-03 12:28:49 +0300

    Change folder icon with a material icon

--- visual_python/explorer/__init__.py
+++ visual_python/explorer/__init__.py
@@ -22,7 +22,7 @@ class Explorer(ttk.Treeview):
 		# Create icons of explorer items
 		self.explorer_file_image = tk.PhotoImage(file='file.png')
 		self.explorer_python_file_image = tk.PhotoImage(file='python.png')
-		self.explorer_folder_image = tk.PhotoImage(file='folder.gif')
+		self.explorer_folder_image = tk.PhotoImage(file='folder.png')
 		# Create context menu for explorer
 		self.folder_menu = tk.Menu(self.window)
 		self.folder_menu.add_command(label='New File', command=self.new_file)
