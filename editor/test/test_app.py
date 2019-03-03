6abafd0d2c22770e1abb68e4374ac66228783f74
İsmail Arılık <arilik.ismail@gmail.com>
2019-03-03 19:09:58 +0300

    Test if app is a Tk instance

--- editor/test/test_app.py
+++ editor/test/test_app.py
@@ -1,9 +1,13 @@
 import unittest
+import tkinter as tk
 from .. import App
 
 class TestApp(unittest.TestCase):
     def setUp(self):
         self.app = App()
+
+    def test_if_it_is_a_tk_instance(self):
+        self.assertIsInstance(self.app, tk.Tk)
 
     def tearDown(self):
         self.app.destroy()
