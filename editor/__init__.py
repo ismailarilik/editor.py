352bc184add18a321e066515f5d4ceddce74d8a6
İsmail Arılık <arilik.ismail@gmail.com>
2019-03-04 15:52:04 +0300

    Set menu

--- editor/__init__.py
+++ editor/__init__.py
@@ -23,28 +23,112 @@ class App(tk.Tk):
         self.icon = tk.PhotoImage(file=self.icon_file_name)
         self.iconphoto(True, self.icon)
 
-        # Create settings component
-        self.settings_component = SettingsComponent()
-
-        # Add menu
+        # Set menu
         self.menu = tk.Menu(self)
         self.config(menu=self.menu)
         # Add file menu
-        self.file_component = FileComponent()
         self.file_menu = tk.Menu(self.menu)
         self.menu.add_cascade(label='File', menu=self.file_menu)
-        self.file_menu.add_command(label='Open File', accelerator='Ctrl+O', command=self.file_component.open_file)
-        self.file_menu.add_command(label='Open Folder', accelerator='Ctrl+D', command=self.file_component.open_folder)
+        self.file_menu.add_command(label='New File', accelerator='Ctrl+N', command=self.new_file_command)
+        self.file_menu.add_command(label='New Folder', accelerator='Ctrl+Shift+N', command=self.new_folder_command)
         self.file_menu.add_separator()
-        self.file_menu.add_command(label='Save File', accelerator='Ctrl+S', command=self.file_component.save_file)
-        self.file_menu.add_command(label='Save File as...', accelerator='Ctrl+Shift+S', command=self.file_component.save_file_as)
+        self.file_menu.add_command(label='Open File', accelerator='Ctrl+O', command=self.open_file_command)
+        self.file_menu.add_command(label='Open Folder', accelerator='Ctrl+Shift+O', command=self.open_folder_command)
         self.file_menu.add_separator()
-        self.file_menu.add_command(label='Quit', accelerator='Ctrl+Q', command=self.quit)
+        self.file_menu.add_command(label='Save File', accelerator='Ctrl+S', command=self.save_file_command)
+        self.file_menu.add_command(label='Save File as', accelerator='Ctrl+Shift+S', command=self.save_file_as_command)
+        self.file_menu.add_command(label='Save All', accelerator='Ctrl+Alt+S', command=self.save_all_command)
+        self.file_menu.add_separator()
+        self.file_menu.add_command(label='Settings', accelerator='Ctrl+,', command=self.settings_command)
+        self.file_menu.add_separator()
+        self.file_menu.add_command(label='Close Editor', accelerator='Ctrl+F4', command=self.close_editor_command)
+        self.file_menu.add_command(label='Close Folder', accelerator='Ctrl+Shift+F4', command=self.close_folder_command)
+        self.file_menu.add_separator()
+        self.file_menu.add_command(label='Quit', accelerator='Ctrl+Q', command=self.quit_command)
         # Add edit menu
-        self.edit_component = EditComponent()
         self.edit_menu = tk.Menu(self.menu)
         self.menu.add_cascade(label='Edit', menu=self.edit_menu)
-        self.edit_menu.add_command(label='Find', accelerator='Ctrl+F', command=self.edit_component.find)
+        self.edit_menu.add_command(label='Undo', accelerator='Ctrl+Z', command=self.undo_command)
+        self.edit_menu.add_command(label='Redo', accelerator='Ctrl+Shift+Z', command=self.redo_command)
+        self.edit_menu.add_separator()
+        self.edit_menu.add_command(label='Select All', accelerator='Ctrl+A', command=self.select_all_command)
+        self.edit_menu.add_separator()
+        self.edit_menu.add_command(label='Cut', accelerator='Ctrl+X', command=self.cut_command)
+        self.edit_menu.add_command(label='Copy', accelerator='Ctrl+C', command=self.copy_command)
+        self.edit_menu.add_command(label='Paste', accelerator='Ctrl+V', command=self.paste_command)
+        self.edit_menu.add_separator()
+        self.edit_menu.add_command(label='Find', accelerator='Ctrl+F', command=self.find_command)
+        self.edit_menu.add_command(label='Replace', accelerator='Ctrl+R', command=self.replace_command)
+        self.edit_menu.add_separator()
+        self.edit_menu.add_command(label='Find in Files', accelerator='Ctrl+Shift+F',
+            command=self.find_in_files_command)
+        self.edit_menu.add_command(label='Replace in Files', accelerator='Ctrl+Shift+R',
+            command=self.replace_in_files_command)
+        # Add view menu
+        self.view_menu = tk.Menu(self.menu)
+        self.menu.add_cascade(label='View', menu=self.view_menu)
+        self.view_menu.add_command(label='Explorer', accelerator='Ctrl+Shift+E', command=self.explorer_command)
+        self.view_menu.add_command(label='Search', accelerator='Ctrl+Shift+F', command=self.search_command)
+        self.view_menu.add_command(label='Version Control', accelerator='Ctrl+Shift+V',
+            command=self.version_control_command)
+        self.view_menu.add_command(label='Debug', accelerator='Ctrl+Shift+D', command=self.debug_command)
+        self.view_menu.add_command(label='Extensions', accelerator='Ctrl+Shift+X', command=self.extensions_command)
+        self.view_menu.add_separator()
+        self.view_menu.add_command(label='Problems', accelerator='Ctrl+Shift+P', command=self.problems_command)
+        self.view_menu.add_command(label='Output', accelerator='Ctrl+Shift+U', command=self.output_command)
+        self.view_menu.add_command(label='Debug Console', accelerator='Ctrl+Shift+C',
+            command=self.debug_console_command)
+        self.view_menu.add_command(label='Terminal', accelerator='Ctrl+Shift+T', command=self.terminal_command)
+        # Add go menu
+        self.go_menu = tk.Menu(self.menu)
+        self.menu.add_cascade(label='Go', menu=self.go_menu)
+        self.go_menu.add_command(label='Next Editor', accelerator='Ctrl+PageDown', command=self.next_editor_command)
+        self.go_menu.add_command(label='Previous Editor', accelerator='Ctrl+PageUp',
+            command=self.previous_editor_command)
+        self.go_menu.add_separator()
+        self.go_menu.add_command(label='Go to File', accelerator='Ctrl+P', command=self.go_to_file_command)
+        self.go_menu.add_separator()
+        self.go_menu.add_command(label='Go to Declaration', accelerator='Ctrl+D',
+            command=self.go_to_declaration_command)
+        self.go_menu.add_separator()
+        self.go_menu.add_command(label='Go to Line/Column', accelerator='Ctrl+G',
+            command=self.go_to_line_column_command)
+        # Add debug menu
+        self.debug_menu = tk.Menu(self.menu)
+        self.menu.add_cascade(label='Debug', menu=self.debug_menu)
+        self.debug_menu.add_command(label='Start Debugging', accelerator='F5', command=self.start_debugging_command)
+        self.debug_menu.add_command(label='Start without Debugging', accelerator='Ctrl+F5',
+            command=self.start_without_debugging_command)
+        self.debug_menu.add_command(label='Stop Debugging', accelerator='Shift+F5', command=self.stop_debugging_command)
+        self.debug_menu.add_command(label='Restart Debugging', accelerator='Ctrl+Shift+F5',
+            command=self.restart_debugging_command)
+        self.debug_menu.add_separator()
+        self.debug_menu.add_command(label='Step Over', accelerator='F10', command=self.step_over_command)
+        self.debug_menu.add_command(label='Step Into', accelerator='F11', command=self.step_into_command)
+        self.debug_menu.add_command(label='Step Out', accelerator='Shift+F11', command=self.step_out_command)
+        self.debug_menu.add_command(label='Continue', accelerator='F5', command=self.continue_command)
+        self.debug_menu.add_separator()
+        self.debug_menu.add_command(label='Toggle Breakpoint', accelerator='F9', command=self.toggle_breakpoint_command)
+        self.debug_menu.add_separator()
+        self.debug_menu.add_command(label='Enable All Breakpoints', command=self.enable_all_breakpoints_command)
+        self.debug_menu.add_command(label='Disable All Breakpoints', command=self.disable_all_breakpoints_command)
+        self.debug_menu.add_command(label='Remove All Breakpoints', command=self.remove_all_breakpoints_command)
+        # Add help menu
+        self.help_menu = tk.Menu(self.menu)
+        self.menu.add_cascade(label='Help', menu=self.help_menu)
+        self.help_menu.add_command(label='Report Issue', command=self.report_issue_command)
+        self.help_menu.add_separator()
+        self.help_menu.add_command(label='View License', command=self.view_license_command)
+        self.help_menu.add_separator()
+        self.help_menu.add_command(label='Check for Updates', command=self.check_for_updates_command)
+        self.help_menu.add_separator()
+        self.help_menu.add_command(label='About', command=self.about_command)
+
+        # Create components
+        self.file_component = FileComponent()
+        self.edit_component = EditComponent()
+        self.settings_component = SettingsComponent()
+
         # Create main frame
         main_frame = tk.Frame(self)
         main_frame.pack(fill=tk.BOTH, expand=True)
@@ -83,7 +167,7 @@ class App(tk.Tk):
         # Add keyboard bindings
         self._add_keyboard_bindings()
         # Register delete window protocol to save unsaved changes and handle other things properly on quit
-        self.protocol('WM_DELETE_WINDOW', self.quit)
+        self.protocol('WM_DELETE_WINDOW', self.quit_command)
 
     def _post_init(self):
         self.file_component.post_init(self.explorer, self.editor, self.set_title)
@@ -102,6 +186,160 @@ class App(tk.Tk):
         if folder_name is not None:
             self._title.folder_name = folder_name
         self.title(str(self._title))
+
+    def new_file_command(self):
+        pass
+
+    def new_folder_command(self):
+        pass
+
+    def open_file_command(self):
+        self.file_component.open_file()
+
+    def open_folder_command(self):
+        self.file_component.open_folder()
+
+    def save_file_command(self):
+        self.file_component.save_file()
+
+    def save_file_as_command(self):
+        self.file_component.save_file_as()
+
+    def save_all_command(self):
+        pass
+
+    def settings_command(self):
+        pass
+
+    def close_editor_command(self):
+        pass
+
+    def close_folder_command(self):
+        pass
+
+    def quit_command(self, event=None):
+        if self.file_component.save_unsaved_changes():
+            self.destroy()
+
+    def undo_command(self):
+        pass
+
+    def redo_command(self):
+        pass
+
+    def select_all_command(self):
+        pass
+
+    def cut_command(self):
+        pass
+
+    def copy_command(self):
+        pass
+
+    def paste_command(self):
+        pass
+
+    def find_command(self):
+        self.edit_component.find()
+
+    def replace_command(self):
+        pass
+
+    def find_in_files_command(self):
+        pass
+
+    def replace_in_files_command(self):
+        pass
+
+    def explorer_command(self):
+        pass
+
+    def search_command(self):
+        pass
+
+    def version_control_command(self):
+        pass
+
+    def debug_command(self):
+        pass
+
+    def extensions_command(self):
+        pass
+
+    def problems_command(self):
+        pass
+
+    def output_command(self):
+        pass
+
+    def debug_console_command(self):
+        pass
+
+    def terminal_command(self):
+        pass
+
+    def next_editor_command(self):
+        pass
+
+    def previous_editor_command(self):
+        pass
+
+    def go_to_file_command(self):
+        pass
+
+    def go_to_declaration_command(self):
+        pass
+
+    def go_to_line_column_command(self):
+        pass
+
+    def start_debugging_command(self):
+        pass
+
+    def start_without_debugging_command(self):
+        pass
+
+    def stop_debugging_command(self):
+        pass
+
+    def restart_debugging_command(self):
+        pass
+
+    def step_over_command(self):
+        pass
+
+    def step_into_command(self):
+        pass
+
+    def step_out_command(self):
+        pass
+
+    def continue_command(self):
+        pass
+
+    def toggle_breakpoint_command(self):
+        pass
+
+    def enable_all_breakpoints_command(self):
+        pass
+
+    def disable_all_breakpoints_command(self):
+        pass
+
+    def remove_all_breakpoints_command(self):
+        pass
+
+    def report_issue_command(self):
+        pass
+
+    def view_license_command(self):
+        pass
+
+    def check_for_updates_command(self):
+        pass
+
+    def about_command(self):
+        pass
 
     def _resize_and_center(self):
         '''
@@ -130,9 +368,5 @@ class App(tk.Tk):
         self.bind('<Control-Shift-KeyPress-s>', self.file_component.save_file_as)
         self.bind('<Control-Shift-KeyPress-S>', self.file_component.save_file_as)
         # Add keyboard bindings for quitting
-        self.bind('<Control-KeyPress-q>', self.quit)
-        self.bind('<Control-KeyPress-Q>', self.quit)
-
-    def quit(self, event=None):
-        if self.file_component.save_unsaved_changes():
-            self.destroy()
+        self.bind('<Control-KeyPress-q>', self.quit_command)
+        self.bind('<Control-KeyPress-Q>', self.quit_command)
