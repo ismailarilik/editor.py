cd8b06c48164e03b7ac07badc80fb6b0c4ab8cf8
İsmail Arılık <arilik.ismail@gmail.com>
2019-07-18 18:16:04 +0300

    Refactor

--- visual_python/title.py
+++ visual_python/title.py
@@ -1,44 +1,48 @@
 class Title:
-    def __init__(self, app_name, *, unsaved_changes_specifier='*', is_there_unsaved_change=False, file_name=None, folder_name=None):
-        self.__app_name = app_name
+    def __init__(self, application_name, unsaved_changes_specifier='*', is_there_unsaved_change=False, file_name=None, folder_name=None):
+        self.__application_name = application_name
         self.__unsaved_changes_specifier = unsaved_changes_specifier
         self.__is_there_unsaved_change = is_there_unsaved_change
         self.__file_name = file_name
         self.__folder_name = folder_name
 
     @property
+    def unsaved_changes_specifier(self):
+        return self.__unsaved_changes_specifier
+
+    @property
     def is_there_unsaved_change(self):
         return self.__is_there_unsaved_change
 
     @is_there_unsaved_change.setter
-    def is_there_unsaved_change(self, value):
-        self.__is_there_unsaved_change = value
+    def is_there_unsaved_change(self, is_there_unsaved_change):
+        self.__is_there_unsaved_change = is_there_unsaved_change
 
     @property
     def file_name(self):
         return self.__file_name
 
     @file_name.setter
-    def file_name(self, value):
-        self.__file_name = value
+    def file_name(self, file_name):
+        self.__file_name = file_name
 
     @property
     def folder_name(self):
         return self.__folder_name
 
     @folder_name.setter
-    def folder_name(self, value):
-        self.__folder_name = value
+    def folder_name(self, folder_name):
+        self.__folder_name = folder_name
 
     def __str__(self):
         title = ''
 
         if self.is_there_unsaved_change:
-            title += self.__unsaved_changes_specifier
+            title += self.unsaved_changes_specifier
         if self.file_name:
             title += f'{self.file_name} - '
         if self.folder_name:
             title += f'{self.folder_name} - '
-        title += self.__app_name
+        title += self.__application_name
 
         return title
