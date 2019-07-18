68b3910b7ce715c9be0a61b4b2206cdf32539375
İsmail Arılık <arilik.ismail@gmail.com>
2019-07-18 18:31:57 +0300

    Test if default unsaved changes specifier is asterisk

--- visual_python/test/test_title.py
+++ visual_python/test/test_title.py
@@ -7,26 +7,30 @@ class TestTitle(unittest.TestCase):
         self.file_name = 'file'
         self.folder_name = 'folder'
         self.separator = ' - '
-        self.app_name = 'Visual Python'
-        self.title = Title(self.app_name, unsaved_changes_specifier=self.unsaved_changes_specifier)
+        self.application_name = 'Visual Python'
+        self.title = Title(self.application_name, unsaved_changes_specifier=self.unsaved_changes_specifier)
+
+    def test_if_default_unsaved_changes_specifier_is_asterisk(self):
+        title = Title(self.application_name)
+        self.assertEqual(title.unsaved_changes_specifier, '*')
 
     def test_title_string(self):
-        self.assertEqual(str(self.title), self.app_name)
+        self.assertEqual(str(self.title), self.application_name)
 
     def test_title_string_with_unsaved_changes(self):
         self.title.is_there_unsaved_change = True
-        self.assertEqual(str(self.title), f'{self.unsaved_changes_specifier}{self.app_name}')
+        self.assertEqual(str(self.title), f'{self.unsaved_changes_specifier}{self.application_name}')
 
     def test_title_string_with_file_name(self):
         self.title.file_name = self.file_name
-        self.assertEqual(str(self.title), f'{self.file_name}{self.separator}{self.app_name}')
+        self.assertEqual(str(self.title), f'{self.file_name}{self.separator}{self.application_name}')
 
     def test_title_string_with_folder_name(self):
         self.title.folder_name = self.folder_name
-        self.assertEqual(str(self.title), f'{self.folder_name}{self.separator}{self.app_name}')
+        self.assertEqual(str(self.title), f'{self.folder_name}{self.separator}{self.application_name}')
 
     def test_title_string_with_unsaved_changes_and_file_name_and_folder_name(self):
         self.title.is_there_unsaved_change = True
         self.title.file_name = self.file_name
         self.title.folder_name = self.folder_name
-        self.assertEqual(str(self.title), f'{self.unsaved_changes_specifier}{self.file_name}{self.separator}{self.folder_name}{self.separator}{self.app_name}')
+        self.assertEqual(str(self.title), f'{self.unsaved_changes_specifier}{self.file_name}{self.separator}{self.folder_name}{self.separator}{self.application_name}')
