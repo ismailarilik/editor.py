45e8c886547922c707602478f9226b81fead6339
İsmail Arılık <arilik.ismail@gmail.com>
2019-08-19 14:34:19 +0300

    setUp VisualTkinter test

--- visual_python/test/test_visual_tkinter.py
+++ visual_python/test/test_visual_tkinter.py
@@ -1,4 +1,7 @@
 import unittest
+from ..visual_tkinter import VisualTkinter
 
 class TestVisualTkinter(unittest.TestCase):
     pass
+    def setUp(self):
+        self.visual_tkinter = VisualTkinter()
