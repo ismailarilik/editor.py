3cc5cd9f674ddbc63847c358601b79494de387ba
İsmail Arılık <arilik.ismail@gmail.com>
2019-08-19 10:04:53 +0300

    Refactor unittests

--- visual_python/test/test_application.py
+++ visual_python/test/test_application.py
@@ -26,36 +26,38 @@ class TestApplication(unittest.TestCase):
         second_menu_label = self.application.menu.entrycget(2, 'label')
         self.assertEqual(second_menu_label, 'Edit')
 
-    def test_if_first_file_menu_command_is_open_file(self):
     def test_if_first_file_menu_entry_is_a_command(self):
         first_file_menu_entry_type = self.application.file_menu.type(1)
         self.assertEqual(first_file_menu_entry_type, 'command')
 
+    def test_if_open_file_command_label_is_correct(self):
         first_file_menu_command_label = self.application.file_menu.entrycget(1, 'label')
         self.assertEqual(first_file_menu_command_label, 'Open File')
 
-    def test_if_open_file_accelerator_is_ctrl_o(self):
-        open_file_accelerator = self.application.file_menu.entrycget('Open File', 'accelerator')
-        self.assertEqual(open_file_accelerator, 'Ctrl+O')
+    def test_if_open_file_command_accelerator_is_ctrl_o(self):
+        open_file_command_accelerator = self.application.file_menu.entrycget('Open File', 'accelerator')
+        self.assertEqual(open_file_command_accelerator, 'Ctrl+O')
 
-    def test_if_open_file_command_is_right(self):
-        open_file_command = self.application.file_menu.entrycget('Open File', 'command')
-        self.assertIn('open_file', open_file_command)
+    def test_if_open_file_command_command_is_correct(self):
+        open_file_command_command = self.application.file_menu.entrycget('Open File', 'command')
+        self.assertIn('open_file', open_file_command_command)
 
-    def test_if_second_file_menu_command_is_open_folder(self):
-        second_file_menu_command_label = self.application.file_menu.entrycget(2, 'label')
-        self.assertEqual(second_file_menu_command_label, 'Open Folder')
     def test_if_second_file_menu_entry_is_a_command(self):
         second_file_menu_entry_type = self.application.file_menu.type(2)
         self.assertEqual(second_file_menu_entry_type, 'command')
 
-    def test_if_open_folder_accelerator_is_ctrl_shift_o(self):
-        open_folder_accelerator = self.application.file_menu.entrycget('Open Folder', 'accelerator')
-        self.assertEqual(open_folder_accelerator, 'Ctrl+Shift+O')
+    def test_if_open_folder_command_label_is_correct(self):
+        open_folder_command_label = self.application.file_menu.entrycget(2, 'label')
+        self.assertEqual(open_folder_command_label, 'Open Folder')
 
-    def test_if_open_folder_command_is_right(self):
-        open_folder_command = self.application.file_menu.entrycget('Open Folder', 'command')
-        self.assertIn('open_folder', open_folder_command)
+    def test_if_open_folder_command_accelerator_is_ctrl_shift_o(self):
+        open_folder_command_accelerator = self.application.file_menu.entrycget('Open Folder', 'accelerator')
+        self.assertEqual(open_folder_command_accelerator, 'Ctrl+Shift+O')
+
+    def test_if_open_folder_command_command_is_correct(self):
+        open_folder_command_command = self.application.file_menu.entrycget('Open Folder', 'command')
+        self.assertIn('open_folder', open_folder_command_command)
+
     def test_if_third_file_menu_entry_is_a_separator(self):
         third_file_menu_entry_type = self.application.file_menu.type(3)
         self.assertEqual(third_file_menu_entry_type, 'separator')
