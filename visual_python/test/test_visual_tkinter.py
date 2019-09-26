d686dbe3ed92a67ba6c6b219b4d257a62ebea402
İsmail Arılık <arilik.ismail@gmail.com>
2019-09-26 20:58:25 +0300

    Remove tests

--- visual_python/test/test_visual_tkinter.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from ..visual_tkinter import VisualTkinter
-
-class TestVisualTkinter(unittest.TestCase):
-    pass
-    def setUp(self):
-        self.visual_tkinter = VisualTkinter()
